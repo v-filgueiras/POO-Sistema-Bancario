9b417283b15df71c1b5be37b59d5e7eb866a4a68
v-filgueiras <vfilgueiras.dev@gmail.com>
2026-03-02 11:54:28 -0400

    exemplos de execucao

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        ContaBancaria conta1 = new ContaBancaria("Victor", "102031");
+
+        conta1.depositar(1000.0);
+        conta1.sacar(732.1);
+
+        System.out.println(conta1.consultarSaldo());
+        System.out.println(conta1.mostrarDados());
     }
 }
