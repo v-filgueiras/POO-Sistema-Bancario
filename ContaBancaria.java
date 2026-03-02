b4c90e12b22081783b17275ea4cf0d4ffa0007e0
v-filgueiras <vfilgueiras.dev@gmail.com>
2026-03-02 11:53:55 -0400

    criando metodos do sistema

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -8,4 +8,37 @@ public class ContaBancaria {
         this.numeroConta = numeroConta;
         this.saldoConta = 0;
     }
+
+    public void depositar(double deposito) {
+        if (deposito <= 0 ) {
+            throw new IllegalArgumentException("O valor deve ser maior que zero.");
+        }
+        else {
+            this.saldoConta = this.saldoConta + deposito;
+        }
+    }
+
+    public void sacar(double saque) {
+        if (saque > this.saldoConta) {
+            throw new IllegalArgumentException("O valor deve ser menor ou igual ao saldo da conta.");
+        }
+
+        if (saque <= 0) {
+            throw new IllegalArgumentException("O valor de saque deve ser superior a zero.");
+        }
+
+        else {
+            this.saldoConta = this.saldoConta - saque;
+        }
+    }
+
+    public double consultarSaldo() {
+        return saldoConta;
+    }
+
+    public String mostrarDados() { 
+        return "Nome: " + this.titular + "\n" + 
+               "Conta: " + this.numeroConta + "\n" + 
+               "Saldo: " + this.saldoConta; 
+    }
 }
